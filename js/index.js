21423a992738ee9abba4731f5bbefcdfb6da7308
Deletoille-Elizalde Iban <139128515+kintergo@users.noreply.github.com>
2025-02-18 08:44:01 +0100

    iban

---
+++ js/index.js
@@ -0,0 +1,33 @@
+//création des page
+
+/**
+ * création de la navBar
+ */
+function navBar(){
+
+}
+
+/**
+ * cette fonction permet d'afficher un contenue d'érreur
+ */
+function displayError(){
+
+}
+
+
+/**
+ * cette fonction permet d'initialiser le site
+ */
+function init(){
+
+}
+
+
+//lancement du script
+try{
+
+    init()
+
+}catch(error){
+    displayError();
+}
